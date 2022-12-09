9455657a0e579eb3adc656a4b857e327f87ab855
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2022-12-09 13:39:16 +0000

    part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,22 +1,22 @@
 use std::{fs, collections::HashSet};
 
 fn main() {
-    let num_knots = 2;
+    let num_knots = 10;
     let mut knots: Vec<(i32, i32)> = Vec::new();
     for _ in 0..num_knots{
         knots.push((0, 0))
     }
     let mut tail_positions: HashSet::<(i32, i32)> = HashSet::new();
 
-    let lines = fs::read_to_string("input.txt").expect("File not found");
+    let lines = fs::read_to_string("test_input_2.txt").expect("File not found");
     let iter = lines.split("\n");
     for line in iter {
         let mut iter = line.splitn(2, " ");
         let direction = iter.next().unwrap();
-        let steps = iter.next().unwrap();
-        let steps_int: u8 = steps.parse().unwrap();
+        let steps_str = iter.next().unwrap();
+        let steps: u8 = steps_str.parse().unwrap();
 
-        // println!("\n== {direction} {steps} ==\n");
+        println!("\n== {direction} {steps} ==\n");
 
         let increment: (i32, i32) = match direction {
             "R" => (1, 0),
@@ -27,13 +27,13 @@ fn main() {
         };
 
         // find H position after it has followed directions
-        for _step in 0..steps_int{
+        for _step in 0..steps{
             //  move head
             knots[0].0 += increment.0;
             knots[0].1 += increment.1;
 
-            // move tail
-            for i in 1..num_knots {
+            // move all other knots
+            for i in 1..knots.len() {
                 let current_knot = knots[i];
                 let previous_knot = knots[i - 1];
                 knots[i] = match (previous_knot.0 - current_knot.0, previous_knot.1 - current_knot.1) {
@@ -58,23 +58,17 @@ fn main() {
                     (x, y) => (current_knot.0 + (x / x.abs()), current_knot.1 + (y / y.abs())),
                 };
             }
-            
-            // println!("tail is at {:?}", tail);
             tail_positions.insert(*knots.last().unwrap());
-
-            // print_grid(&knots);
-            // println!("head position {:?}", head);
         }
+        print_grid(&knots);
     }
-    // println!("{:?}\n", tail_positions);
-    println!("there are {:?} unique positions", tail_positions.len());
+    println!("there are {:?} unique tail positions", tail_positions.len());
 }
 
 fn print_grid(knots: &Vec<(i32, i32)>) {
-    let grid_size = 6;
     print!("\n");
-    for y in (0..grid_size).rev() {
-        for x in 0..grid_size {
+    for y in (-15..15).rev() {
+        for x in -15..15 {
             if knots.contains(&(x, y)) {
                 let p = &knots.iter().position(|&(a, b)| (x, y) == (a, b)).unwrap();
                 print!("{p}")
