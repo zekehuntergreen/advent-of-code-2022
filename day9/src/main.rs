5008f7dee8e628264005706964b652d8852d7e09
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2022-12-09 17:10:20 +0000

    simplify match to find tail position

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     }
     let mut tail_positions: HashSet::<(i32, i32)> = HashSet::new();
 
-    let lines = fs::read_to_string("test_input_2.txt").expect("File not found");
+    let lines = fs::read_to_string("input.txt").expect("File not found");
     let iter = lines.split("\n");
     for line in iter {
         let mut iter = line.splitn(2, " ");
@@ -16,7 +16,7 @@ fn main() {
         let steps_str = iter.next().unwrap();
         let steps: u8 = steps_str.parse().unwrap();
 
-        println!("\n== {direction} {steps} ==\n");
+        // println!("\n== {direction} {steps} ==\n");
 
         let increment: (i32, i32) = match direction {
             "R" => (1, 0),
@@ -26,7 +26,6 @@ fn main() {
             _ => panic!("Unknown direction"),
         };
 
-        // find H position after it has followed directions
         for _step in 0..steps{
             //  move head
             knots[0].0 += increment.0;
@@ -37,30 +36,13 @@ fn main() {
                 let current_knot = knots[i];
                 let previous_knot = knots[i - 1];
                 knots[i] = match (previous_knot.0 - current_knot.0, previous_knot.1 - current_knot.1) {
-                    // If the head is ever two steps directly up, down, left, or right from the tail,
-                    // the tail must also move one step in that direction so it remains
-                    // close enough
-                    (2, 0) => (current_knot.0 + 1, current_knot.1), // right
-                    (0, 2) => (current_knot.0, current_knot.1 + 1), // up
-                    (-2, 0) => (current_knot.0 - 1, current_knot.1), // left
-                    (0, -2) => (current_knot.0, current_knot.1 -1), // down
-                    // Otherwise, if the head and tail aren't touching 
-                    (0, 0) => current_knot, // same
-                    // and aren't in the same row or column, 
-                    (_, 0) => current_knot,
-                    (0, _) => current_knot,
-                    // (and aren't diagonally adjacent)
-                    (1, 1) => current_knot,
-                    (-1, 1) => current_knot,
-                    (1, -1) => current_knot,
-                    (-1, -1) => current_knot,
-                    // the tail always moves one step diagonally to keep up
-                    (x, y) => (current_knot.0 + (x / x.abs()), current_knot.1 + (y / y.abs())),
+                    (-1..=1, -1..=1) => current_knot,
+                    (x, y) => (current_knot.0 + x.signum(), current_knot.1 + y.signum()),
                 };
             }
             tail_positions.insert(*knots.last().unwrap());
         }
-        print_grid(&knots);
+        // print_grid(&knots);
     }
     println!("there are {:?} unique tail positions", tail_positions.len());
 }
