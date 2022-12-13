469fc2dfca4525aa9152988a2755cfc855e7aeab
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2022-12-13 19:08:11 +0000

    part2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -6,30 +6,32 @@ fn main() {
     let mut x = 1;
     let mut i = 1;
     let mut maybe_command: Option<&str> = commands.next();
-    let mut total = 0;
     while maybe_command != None {
         let command = maybe_command.unwrap();
         maybe_command = commands.next();
 
-        if command != "noop" {
-            (i, total) = increment_i(i, x, total);
+        if command == "noop" {
+            i = draw(i, x);
+        } else {
+            i = draw(i, x);
+            i = draw(i, x);
             let mut s = command.splitn(2, " ");
             let _op = s.next().unwrap();
             let num: i32= s.next().expect("no arg").trim().parse().expect("nan");
             x += num;
         }
-        (i, total) = increment_i(i, x, total);
     }
-    println!("{total}")
 }
 
-fn increment_i(mut i: i32, x: i32, mut total: i32) -> (i32, i32){
-    i += 1;
-    // println!("incremented i to {i}");
-    if i % 40 == 20 && i <= 220  {
-        let signal_strength = x * i;
-        println!("register X has the value {x}, so the signal strength is {i} * {x} = {signal_strength}");
-        total += signal_strength;
+fn draw(i: i32, x: i32) -> i32 {
+    let position = (i - 1) % 40;
+    if x - 1 <= position && position <= x + 1 {
+        print!("#")
+    } else {
+        print!(".")
     }
-    return (i, total)
+    if i % 40 == 0 {
+        print!("\n")
+    }
+    return i + 1;
 }
