cc04345a6d24c677e176c8b3311c77a8840644d0
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2022-12-14 19:49:37 +0000

    cleanup

--- day14/day14.py
+++ day14/day14.py
@@ -1,31 +1,23 @@
-puzzle_input = open("input.txt").readlines()
+source, rocks, sand = (500, 0), set(), set()
 
 
-source = (500, 0)
-rocks = set()
-sand = set()
-
-for line in puzzle_input:
+for line in open("input.txt").readlines():
     points = line.split(" -> ")
     for i in range(len(points) - 1):
-        start, end = points[i], points[i + 1]
         start_x, start_y, end_x, end_y = list(
-            map(int, start.split(",") + end.split(","))
+            map(int, points[i].split(",") + points[i + 1].split(","))
         )
-
         for j in range(min(start_x, end_x), max(start_x, end_x) + 1):
             for k in range(min(start_y, end_y), max(start_y, end_y) + 1):
                 rocks.add((j, k))
 
 
 max_y = max([y for _, y in rocks])
-min_x = min([x for x, _ in rocks])
-max_x = max([x for x, _ in rocks])
 
 
 def print_grid(current=None):
     for y in range(max_y + 2):
-        for x in range(min_x - 1, max_x + 2):
+        for x in range(min([x for x, _ in rocks]) - 1, max([x for x, _ in rocks]) + 2):
             if (x, y) == source:
                 print("+", end="")
             elif (x, y) in rocks:
@@ -41,11 +33,12 @@ at_rest = False
 current = source
 while True:
     if not at_rest:
-        if (current[0], current[1] + 1) not in rocks.union(sand):
+        rocks_and_sand = rocks.union(sand)
+        if (current[0], current[1] + 1) not in rocks_and_sand:
             current = (current[0], current[1] + 1)
-        elif (current[0] - 1, current[1] + 1) not in rocks.union(sand):
+        elif (current[0] - 1, current[1] + 1) not in rocks_and_sand:
             current = (current[0] - 1, current[1] + 1)
-        elif (current[0] + 1, current[1] + 1) not in rocks.union(sand):
+        elif (current[0] + 1, current[1] + 1) not in rocks_and_sand:
             current = (current[0] + 1, current[1] + 1)
         else:
             at_rest = True
