f6d9f67b5ec540592bbc79721c4342f231d16c32
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2022-12-14 20:26:58 +0000

    part 2

--- day14/day14.py
+++ day14/day14.py
@@ -1,5 +1,5 @@
 source, rocks, sand = (500, 0), set(), set()
-
+part1 = False
 
 for line in open("input.txt").readlines():
     points = line.split(" -> ")
@@ -16,14 +16,16 @@ max_y = max([y for _, y in rocks])
 
 
 def print_grid(current=None):
-    for y in range(max_y + 2):
-        for x in range(min([x for x, _ in rocks]) - 1, max([x for x, _ in rocks]) + 2):
-            if (x, y) == source:
+    for y in range(max_y + 4):
+        rock_x = [x for x, _ in rocks]
+        min_x, max_x = min(rock_x) - 10, max(rock_x) + 10
+        for x in range(min_x, max_x):
+            if (x, y) in sand or (x, y) == current:
+                print("o", end="")
+            elif (x, y) == source:
                 print("+", end="")
             elif (x, y) in rocks:
                 print("#", end="")
-            elif (x, y) in sand or (x, y) == current:
-                print("o", end="")
             else:
                 print(".", end="")
         print()
@@ -33,22 +35,28 @@ at_rest = False
 current = source
 while True:
     if not at_rest:
-        rocks_and_sand = rocks.union(sand)
-        if (current[0], current[1] + 1) not in rocks_and_sand:
-            current = (current[0], current[1] + 1)
-        elif (current[0] - 1, current[1] + 1) not in rocks_and_sand:
-            current = (current[0] - 1, current[1] + 1)
-        elif (current[0] + 1, current[1] + 1) not in rocks_and_sand:
-            current = (current[0] + 1, current[1] + 1)
+        down = (current[0], current[1] + 1)
+        left = (current[0] - 1, current[1] + 1)
+        right = (current[0] + 1, current[1] + 1)
+        if not part1 and current[1] == max_y + 1:
+            at_rest = True
+        elif down not in rocks and down not in sand:
+            current = down
+        elif left not in rocks and left not in sand:
+            current = left
+        elif right not in rocks and right not in sand:
+            current = right
         else:
             at_rest = True
     else:
         sand.add(current)
+        if not part1 and current == source:
+            break
         current = source
         at_rest = False
 
-    if current[1] > max_y:
+    if part1 and current[1] > max_y:
         break
 
-print_grid(current=current)
+# print_grid(current=current)
 print("\ngrains of sand: ", len(sand))
